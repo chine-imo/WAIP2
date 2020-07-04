61ce722d58267daad671ac8bac67c0969fc8a3a4
Chine Nana <chine_n@yahoo.com>
2020-07-04 12:39:44 +0100

    changes

--- output.tf
+++ output.tf
@@ -1,29 +1,30 @@
 # Jenkins box tag name
-/*output "jenkins_tagName" {
+output "jenkins_tagName" {
   value = aws_instance.jenkins.tags.Name
-}*/
+}
 
 # Jenkins box public ip
-/*output "jenkins_public_ip" {
+output "jenkins_public_ip1" {
   value = aws_instance.jenkins.public_ip
-}*/
+}
 
-output "jenkins_public_ip" {
+output "jenkins_public_ip2" {
   value = "${
     formatlist(
       "%s:%s",
-      aws_instance.jenkins.tags.Name,
+      #aws_instance.jenkins.tags.Name,
+      "JENKINS_PUB_IP",
       aws_instance.jenkins.public_ip
     )
   }"
 }
 
 # Jenkins box private ip
-/*output "jenkins_private_ip" {
+output "jenkins_private_ip1" {
   value = aws_instance.jenkins.private_ip
-}*/
+}
 
-output "jenkins_private_ip" {
+output "jenkins_private_ip2" {
   value = "${
     formatlist(
       "%s:%s",
@@ -34,14 +35,14 @@ output "jenkins_private_ip" {
 }
 
 # nginx tag name 
-/*output "proxy_server_TagName" {
+output "proxy_server_TagName" {
   value = aws_instance.nginx.tags.Name
-}*/
+}
 
 # nginx public ip
-/*output "proxy_server_public_ip" {
+output "proxy_server_public_ip" {
   value = aws_instance.nginx.public_ip
-}*/
+}
 
 output "nginx_public_ip" {
   value = "${
@@ -54,9 +55,9 @@ output "nginx_public_ip" {
 }
 
 # nginx private ip
-/*output "proxy_server_private_ip" {
+output "proxy_server_private_ip" {
   value = aws_instance.nginx.private_ip
-}*/
+}
 
 output "nginx_private_ip" {
   value = "${
@@ -69,14 +70,14 @@ output "nginx_private_ip" {
 }
 
 # app server tag name
-/*output "app_server_TagName" {
+output "app_server_TagName" {
   value = aws_instance.app_machine.*.tags.Name
-}*/
+}
 
 # app server private ip
-/*output "app_server_private_ips" {
+output "app_server_private_ips" {
   value = aws_instance.app_machine.*.private_ip
-}*/
+}
 
 output "app_machines_private_ips" {
   value = "${
@@ -92,7 +93,7 @@ output "app_machines_private_ips" {
 output "app_server_id" {
   value = {
     for instance in aws_instance.app_machine:
-    instance.tags.Name => instance.id
+    instance.tags.Name => instance.private_ip
   }
 }
 
